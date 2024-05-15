5edc6703ba0f3046e9956ec8932a0862288b75e2
Mattia04 <mattia.ballico@gmail.com>
2024-05-15 10:21:39 +0200

    bugfix

--- packages/funcs.py
+++ packages/funcs.py
@@ -146,26 +146,30 @@ class Statistics:
         x_data: np.array, y_data: np.array, sigma_y: np.array = -1
     ) -> Tuple[float]:
         # *
-        def test_func(x, a, b, c, d, e):
-            return a * np.exp(b * x) + c * np.exp(d * x) + e
+        def test_func(x, a, b, c, d, e, phi):
+            return a * np.exp(b * (x - phi)) + c * np.exp(d * (x - phi)) + e
 
         # *
 
         if sigma_y == -1:
             params, params_cov = optimize.curve_fit(
-                test_func, x_data, y_data, p0=[1, -1, 1, -0.0001, 0], maxfev=5000
+                test_func,
+                x_data,
+                y_data,
+                p0=[0.1, -0.005, 1.45, -1e-6, 2, 0],
+                maxfev=10000,
             )
         else:
             params, params_cov = optimize.curve_fit(
                 test_func,
                 x_data,
                 y_data,
-                p0=[1, 1, 0.1, 0.1, 0],
+                p0=[1, 1, 0.1, 0.1, 0, 0],
                 sigma=sigma_y,
                 maxfev=5000,
             )
 
-        return *params, *np.diag(params_cov)
+        return params, np.diag(params_cov)
 
     @staticmethod
     def chi2(expected, observed, sigma) -> Tuple[float]:
